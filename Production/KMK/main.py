234dfd27e2d2dcde4681e8ce7116445e2ec3ce29
Akshit Erukulla <aksheru09@gmail.com>
2025-06-11 11:29:11 -0400

    fixed

--- Production/KMK/main.py
+++ Production/KMK/main.py
@@ -1,43 +1,74 @@
 import board
+import time
+import supervisor
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.keys import KC
 from kmk.modules.encoder import EncoderHandler
 from kmk.modules.layers import Layers
+from kmk.extensions.media_keys import MediaKeys
+from kmk.extensions.mouse_keys import MouseKeys
+from kmk.handlers.sequences import send_string
 
 keyboard = KMKKeyboard()
 encoder_handler = EncoderHandler()
 layers = Layers()
-
 keyboard.modules = [layers, encoder_handler]
 
+keyboard.extensions.append(MediaKeys())
+keyboard.extensions.append(MouseKeys())
+
 keyboard.direct_pins = [
-    board.GPIO6,
-    board.GPIO7,
-    board.GPIO0,
-    board.GPIO1
+    board.GPIO2,  
+    board.GPIO3,  
+    board.GPIO4   
 ]
 
 encoder_handler.pins = ((board.GPIO2, board.GPIO3, board.GPIO1, False),)
 
-_______ = KC.TRNS
-XXXXXXX = KC.NO
-
-MEDIA_MUTE = KC.MUTE
-MEDIA_VOLUP = KC.AUDIO_VOL_UP
-MEDIA_VOLDN = KC.AUDIO_VOL_DOWN
+tab_open = False
+site_url = 'https://www.google.com'
 
 keyboard.keymap = [
     [
-        KC.S,
-        KC.D,
-        KC.E,
-        MEDIA_MUTE
+        KC.NO,  
+        KC.NO,  
+        KC.NO
     ]
 ]
 
 encoder_handler.map = [
-    ((MEDIA_VOLUP, MEDIA_VOLDN, MEDIA_MUTE),)
+    ((KC.AUDIO_VOL_UP, KC.AUDIO_VOL_DOWN, KC.NO),)
 ]
+
+@keyboard.key_pressed
+def on_key_pressed(key):
+    global tab_open
+
+    if key == keyboard.keys[0]: 
+        keyboard.send(KC.LCTRL(KC.C))  
+        time.sleep(0.1)
+        keyboard.send(KC.LCTRL(KC.LSHIFT(KC.Y)))  
+    elif key == keyboard.keys[1]:  
+        if not tab_open:
+            keyboard.send(KC.LGUI(KC.R)) 
+            time.sleep(0.2)
+            keyboard.send(send_string(site_url + "\n"))
+            tab_open = True
+        else:
+            keyboard.send(KC.LCTRL(KC.W))  
+            tab_open = False
+    elif key == keyboard.keys[2]:  
+        print("Starting anti-sleep mouse movement")
+        start_time = time.monotonic()
+        while True:
+            if not supervisor.runtime.serial_connected:  
+                break
+            current = time.monotonic()
+            if current - start_time >= 5:
+                keyboard.mouse.move(x=10, y=0)
+                time.sleep(0.2)
+                keyboard.mouse.move(x=-10, y=0)
+                start_time = current
 
 if __name__ == '__main__':
     keyboard.go()
--- firmware/main.py
+++ firmware/main.py
@@ -14,33 +14,28 @@ encoder_handler = EncoderHandler()
 layers = Layers()
 keyboard.modules = [layers, encoder_handler]
 
-#
 keyboard.extensions.append(MediaKeys())
 keyboard.extensions.append(MouseKeys())
 
-# Pin setup
 keyboard.direct_pins = [
-    board.GPIO2,  # Key 1 - Autocorrect
-    board.GPIO3,  # Key 2 - Website toggle
-    board.GPIO4   # Key 3 - Anti-sleep
+    board.GPIO2,  
+    board.GPIO3,  
+    board.GPIO4   
 ]
 
 encoder_handler.pins = ((board.GPIO2, board.GPIO3, board.GPIO1, False),)
 
-# Toggle state
 tab_open = False
 site_url = 'https://www.google.com'
 
-# Key behavior map
 keyboard.keymap = [
     [
-        KC.NO,  # Key 1 (Autocorrect)
-        KC.NO,  # Key 2 (Website toggle)
-        KC.NO   # Key 3 (Anti-sleep)
+        KC.NO,  
+        KC.NO,  
+        KC.NO
     ]
 ]
 
-# Encoder map (no press mapped)
 encoder_handler.map = [
     ((KC.AUDIO_VOL_UP, KC.AUDIO_VOL_DOWN, KC.NO),)
 ]
@@ -49,24 +44,24 @@ encoder_handler.map = [
 def on_key_pressed(key):
     global tab_open
 
-    if key == keyboard.keys[0]:  # Autocorrect
-        keyboard.send(KC.LCTRL(KC.C))  # Copy selection
+    if key == keyboard.keys[0]: 
+        keyboard.send(KC.LCTRL(KC.C))  
         time.sleep(0.1)
-        keyboard.send(KC.LCTRL(KC.LSHIFT(KC.Y)))  # Google Docs autocorrect
-    elif key == keyboard.keys[1]:  # Website toggle
+        keyboard.send(KC.LCTRL(KC.LSHIFT(KC.Y)))  
+    elif key == keyboard.keys[1]:  
         if not tab_open:
-            keyboard.send(KC.LGUI(KC.R))  # Open run dialog
+            keyboard.send(KC.LGUI(KC.R)) 
             time.sleep(0.2)
             keyboard.send(send_string(site_url + "\n"))
             tab_open = True
         else:
-            keyboard.send(KC.LCTRL(KC.W))  # Close tab
+            keyboard.send(KC.LCTRL(KC.W))  
             tab_open = False
-    elif key == keyboard.keys[2]:  # Anti-sleep
+    elif key == keyboard.keys[2]:  
         print("Starting anti-sleep mouse movement")
         start_time = time.monotonic()
         while True:
-            if not supervisor.runtime.serial_connected:  # Emergency exit
+            if not supervisor.runtime.serial_connected:  
                 break
             current = time.monotonic()
             if current - start_time >= 5:
@@ -77,4 +72,3 @@ def on_key_pressed(key):
 
 if __name__ == '__main__':
     keyboard.go()
-```
